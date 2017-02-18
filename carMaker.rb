c8402771ff844e216e8182eea4f017914180f874
JStehouwer <stay.jb@gmail.com>
2017-02-18 17:08:52 -0500

    hi

--- carMaker.rb
+++ carMaker.rb
@@ -2,6 +2,7 @@ require 'gosu'
 require 'pry'
 
 require_relative 'quad.rb'
+require_relative 'circle.rb'
 
 class CarMaker < Gosu::Window
   def initialize
@@ -23,27 +24,29 @@ class CarMaker < Gosu::Window
     # ...
 
     if button_down?(Gosu::MsLeft)
-			puts(@clickedX1,@clickedY1,@clickedX2,@clickedY2,@clickedX3,@clickedY3)
+			puts("X1: ", @clickedX1, "Y1: ", @clickedY1, "X2 ", @clickedX2, "Y2 ", @clickedY2, "X3 " ,@clickedX3, "Y3 " , @clickedY3, "X4 ", @clickedX4, "Y4", @clickedY4)
 			if @clickedX1 == -1  #have not reset
-				if @clickedX4 != mouse_x
+				if dist(@clickedX4, @clickedY4, mouse_x, mouse_y) > 10
 					@clickedX1 = mouse_x #set to where we are
 					@clickedY1 = mouse_y
+					@clickedX4 = -1;
+					@clickedY4 = -1;
 			end
 
     elsif @clickedX2 == -1 #have not reset x2
-			if @clickedX1 != mouse_x #mouse is not in same place as x1
+			if dist(@clickedX1, @clickedY1, mouse_x, mouse_y) > 10
 				@clickedX2 = mouse_x
 				@clickedY2 = mouse_y
 		end
 
     elsif @clickedX3 == -1 #have not reset
-			if @clickedX2 != mouse_x #mouse not in same place as x2
+			if dist(@clickedX2, @clickedY2, mouse_x, mouse_y) > 10
 				@clickedX3 = mouse_x
 				@clickedY3 = mouse_y
 		end
     
 		elsif @clickedX4 == -1 #have not reset
-			if @clickedX3 != mouse_x #mouse not in same place as x3
+			if dist(@clickedX3, @clickedY3, mouse_x, mouse_y) > 10
 				@clickedX4 = mouse_x
 				@clickedY4 = mouse_y
         @myQuad = Quad.new(@clickedX1,@clickedY1,@clickedX2,@clickedY2,@clickedX3,@clickedY3,@clickedX4, @clickedY4) 
@@ -53,16 +56,20 @@ class CarMaker < Gosu::Window
     		@clickedY2 = -1
     		@clickedX3 = -1
     		@clickedY3 = -1
-    		@clickedX4 = -1
-    		@clickedY4 = -1
+    	
 			end
     end
   end     
  end
 
+	def dist(spotX, spotY, spotX2, spotY2)
+		 Math.sqrt( (spotX - spotX2)*(spotX - spotX2) + (spotY - spotY2)*(spotY - spotY2) )
+	end
+
   def draw
     # ...
     @myQuad.draw
+    
     draw_quad(mouse_x, mouse_y, Gosu::Color.argb(0xffffffff), mouse_x+5, mouse_y+5, Gosu::Color.argb(0xffffffff), mouse_x+5, mouse_y, 0xffffffff, mouse_x, mouse_y+5, Gosu::Color.argb(0xffffffff), z=1, mode = :default)
   end
 end
