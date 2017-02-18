fd27d3d421d9b03056da760025665cb8cdbef8f4
JStehouwer <stay.jb@gmail.com>
2017-02-18 16:13:36 -0500

    Rng track

--- map.rb
+++ map.rb
@@ -3,9 +3,18 @@ require_relative 'track_piece'
 
 class Map
 
-  def initialize(w_height,w_width)
+  def initialize(w_height,w_width,rng,piece_length=300)
+		@x_start = 50
+		@length = piece_length
+		@y_base = w_height-250
+		@y_coords = [@y_base,@y_base]
+		(1..4).each do |i|
+			@y_coords << @y_coords[i-1]+rng.rand(-25..25)
+		end
     @track_pieces = []
-    @track_pieces << TrackPiece.new(50,w_height-250,350,w_height-250)
+		(0..@y_coords.length-2).each do | i |
+    	@track_pieces << TrackPiece.new(@x_start+@length*i,@y_coords[i],@x_start+@length*(i+1),@y_coords[i+1])
+		end
   end
 
   def draw
